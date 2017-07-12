b0184ec30520e9656c2c7aecfab2444a1cc28de9
Christopher Kim <chris.sw.kim@gmail.com>
2017-07-12 17:53:33 -0400

    Add more generic and custom aws cli aliases.

--- setup/aws-alias.sh
+++ setup/aws-alias.sh
@@ -6,6 +6,30 @@ alias aws-ssh='ssh -i ~/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp'
 alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
 alias aws-state='aws ec2 describe-instances --instance-ids $instanceId --query "Reservations[0].Instances[0].State.Name"'
 
+# A more generic alias for retrieving the id, public ip and state of an instance by type (t2.micro, p2.xlarge, etc.)
+aws_get_by_type() {
+  aws ec2 describe-instances --filters "Name=instance-type,Values=$1" --query 'Reservations[*].Instances[*].[InstanceId,PublicIpAddress,State.Name]'
+}
+
+# A more generic alias for retrieving the id, public ip and state of an instance by tag with key=Name
+aws_get_by_name() {
+  aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" --query 'Reservations[*].Instances[*].[InstanceId,PublicIpAddress,State.Name]'
+}
+
+# A more generic ssh alias that accepts an instance ip and an optional path to a certificate file (otherwise uses a default path)
+aws_ssh() {
+  ssh -i ${2:-~/.ssh/aws-key-fast-ai.pem} ec2-user@"$1"
+}
+
+# A more generic start instance alias
+aws_start() {
+  aws ec2 start-instances --instance-ids "$1" && aws ec2 wait instance-running --instance-ids "$1" && echo "we're in business baby!"
+}
+
+# A more generic stop instance alias
+aws_stop() {
+  aws ec2 stop-instances --instance-ids "$1" && aws ec2 wait instance-stopped --instance-ids "$1" && echo "business is closed!"
+}
 
 if [[ `uname` == *"CYGWIN"* ]]
 then
